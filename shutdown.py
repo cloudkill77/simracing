6340d9bfa49fafe33438e6d43572739a8c641c00
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2025-05-31 14:24:15 +0200

    Update shutdown.py
    serial message changed from "os_ready" to "1"

--- shutdown.py
+++ shutdown.py
@@ -6,7 +6,7 @@ ser = serial.Serial('COM5', 9600, timeout=1)
 time.sleep(2)  # Give time for Arduino to reset
 
 # Send "OS_READY" signal
-ser.write(b'OS_READY\n')
+ser.write(b"1\n")
 
 # Optionally, wait for Arduino's ACK
 response = ser.readline().decode('utf-8').strip()
